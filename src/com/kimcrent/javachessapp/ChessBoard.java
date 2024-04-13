ba587306fb434bb46a8cbcc16390ab0108ea5c1e
Roman Kim <kimcrent@gmail.com>
2024-04-13 22:19:47 +0600

    add new class and initial chess board

---
+++ src/com/kimcrent/javachessapp/ChessBoard.java
@@ -0,0 +1,12 @@
+public class ChessBoard {
+    private Piece [][] board;
+    
+    public ChessBoard() {
+        this.board = new Pice[8][8];
+        setupPieces();
+    }
+
+    private void setupPieces() {
+        // Initial setup will be detailed in subsequent snippets
+    }
+}
